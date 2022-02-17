f92527e3322b120165e6cacaf85123a8efb39ed8
faizdecco <faizalidecco264@gmail.com>
2022-02-17 18:46:39 +0500

    Projects::Set up User and Project Association
    * Create ProjectAssignment Model
    
    * Create has_many through association between User and Project using ProjectAssignment

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_17_132721) do
+ActiveRecord::Schema.define(version: 2022_02_17_133920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
+
+  create_table "project_assignments", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_project_assignments_on_project_id"
+    t.index ["user_id"], name: "index_project_assignments_on_user_id"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
@@ -40,4 +49,7 @@ ActiveRecord::Schema.define(version: 2022_02_17_132721) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "project_assignments", "projects"
+  add_foreign_key "project_assignments", "users"
 end
