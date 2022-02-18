c539d3b33f0ab95d94675bb81e5e26856fbfbd02
faizdecco <faizalidecco264@gmail.com>
2022-02-19 00:32:10 +0500

    Bugs::Manage Bugs and Projects Path Helpers
    * Remove bugs related to paths
    
    * Fix Rubocop issues

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Users
+  class RegistrationsController < Devise::RegistrationsController
+    # rubocop:disable Rails/LexicallyScopedActionFilter
+    before_action :configure_sign_up_params, only: [:create]
+    before_action :configure_account_update_params, only: [:update]
+    # rubocop:enable Rails/LexicallyScopedActionFilter
+
+    protected
+
+    # If you have extra params to permit, append them to the sanitizer.
+    def configure_sign_up_params
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
+
+    # If you have extra params to permit, append them to the sanitizer.
+    def configure_account_update_params
+      # devise_parameter_sanitizer.permit(:name, :email, :password, :password_confirmation)
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
+  end
+end
