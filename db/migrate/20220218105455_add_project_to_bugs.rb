2dd00097ec64194ae07ddabd08e50210ca922392
faizdecco <faizalidecco264@gmail.com>
2022-02-18 19:59:17 +0500

    Bugs::Bug and Project Associations
    * Create Project and Bug Association with has_many through
    
    * Create BugAssignment Model
    
    * Manage Project Creation through User

--- db/migrate/20220218105455_add_project_to_bugs.rb
+++ db/migrate/20220218105455_add_project_to_bugs.rb
@@ -1,5 +1,8 @@
+<<<<<<< HEAD
 # frozen_string_literal: true
 
+=======
+>>>>>>> 14ff5ea (Bugs::Bug and Project Associations)
 class AddProjectToBugs < ActiveRecord::Migration[5.2]
   def change
     add_reference :bugs, :project, foreign_key: true
