146f269f52701708976743fdfb626beac72d99f7
faizdecco <faizalidecco264@gmail.com>
2022-02-18 15:14:47 +0500

    Projects::Projects Module Implementation
    * Create Projects Controller
    
    * Manage Projects Routes
    
    * Edit Project Form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root to: "users#index"
+  devise_for :users, :paths => 'users', controllers: { sessions: 'users/sessions' }
+
+  resources :users, only: [:index] do
+    resources :projects
+  end
+
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
 end
