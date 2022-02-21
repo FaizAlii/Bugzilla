4e5a39f17ab62d609a52460ba6440bf7bc8ab08b
faizdecco <faizalidecco264@gmail.com>
2022-02-21 18:06:48 +0500

    Rolify::Create Many to Many relationship between user and roles
    * Generate Role Model
    
    * Create 3 Roles (Manager, QA, Dev) through seed file
    
    * Add callback to assign roles based on checkbox values when user registers

--- db/schema.rb
+++ db/schema.rb
@@ -100,6 +100,16 @@ ActiveRecord::Schema.define(version: 2022_02_25_102925) do
     t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
   end
 
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.string "resource_type"
+    t.bigint "resource_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
