fb29643558a1f4790b23d12d291e10dc0c342b58
faizdecco <faizalidecco264@gmail.com>
2022-02-22 21:59:23 +0500

    Bugs::Manage Enums
    * Create Enums for bug_type and status
    
    * Display dropdowns for in Bug form partial

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -4,4 +4,7 @@ class Bug < ApplicationRecord
   belongs_to :project
   has_many :bug_assignments, dependent: :destroy
   has_many :users, through: :bug_assignments
+
+  enum bug_type: { feature: 0, bug: 1 }
+  enum status: { new: 0, started: 1, completed: 2 }, _prefix: true
 end
