f2cdb4d49b97e827225b22f240ea2c4f509d4d3a
faizdecco <faizalidecco264@gmail.com>
2022-02-25 13:08:35 +0500

    Projects::PR Review Fixes

--- app/models/project_assignment.rb
+++ app/models/project_assignment.rb
@@ -3,4 +3,6 @@
 class ProjectAssignment < ApplicationRecord
   belongs_to :user
   belongs_to :project
+
+  validates :user_id, uniqueness: { scope: :project_id }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, paths: 'users', controllers: { sessions: 'users/sessions' }
+  devise_for :users # , paths: 'users', controllers: { sessions: 'users/sessions' }
 
   resources :users, only: [:index] do
     resources :projects
