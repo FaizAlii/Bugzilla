36f110250929b4eb9f2c5e1dd9dcda1dd857ac94
faizdecco <faizalidecco264@gmail.com>
2022-02-25 16:39:59 +0500

    Rolify::Rebase Conflicts resolved

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   before_action :configure_sign_up_params, only: :create, if: :devise_controller?
+  after_action :set_role, only: %i[create update], if: :devise_controller?
   before_action :configure_account_update_params, only: :update, if: :devise_controller?
 
   def after_sign_in_path_for(_resource)
@@ -14,5 +15,12 @@ class ApplicationController < ActionController::Base
 
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  def set_role
+    @user.roles = []
+    params[:users_roles][:role_ids].each do |role|
+      @user.add_role(Role.find(role).name)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,8 @@ Rails.application.routes.draw do
   resources :bugs
   devise_for :users
 
-  resources :users, only: [:index]
-  get '/users/:user_id/my_bugs', to: 'users#my_bugs', as: 'my_bugs'
-  resources :projects do
-    resources :bugs
+  resources :users, only: [:index] do
+    resources :projects
   end
 
   resources :projects, only: [] do
